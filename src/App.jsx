00187705040c197114ea0b0a6ec8171b021b6894
Sdkrsn <sdkry0110@gmail.com>
2025-02-17 16:48:41 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
-import './App.css'
+import styles from "./App.module.css";
 
 function App() {
 
 
-  return <>Hello world!</>
+  return <div className={styles.App}>Hello world!</div>
 }
 
 export default App
